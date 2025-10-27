bb8065e559d15b982c4fc39fc3de83023f18618c
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2025-10-26 18:12:35 -0600

    Add google services

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
-    id("com.diffplug.spotless") version "8.0.0"
+
+    id("com.diffplug.spotless")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -69,8 +71,18 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
+
+    implementation(libs.credentials)
+    implementation(libs.credentials.play.services.auth)
+    implementation(libs.googleid)
+    implementation(libs.play.services.auth)
+    implementation(libs.legacy.support.v4)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
