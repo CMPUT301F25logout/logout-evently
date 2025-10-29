2438abcd84a2e78e155e6748ca207fc1784bb560
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2025-10-29 13:31:49 -0600

    Example implementation of architecture classes

--- app/src/main/java/com/example/evently/ui/organizer/OrganizerActivity.java
+++ app/src/main/java/com/example/evently/ui/organizer/OrganizerActivity.java
@@ -1,32 +1,12 @@
 package com.example.evently.ui.organizer;
 
-import android.os.Bundle;
+import com.example.evently.R;
+import com.example.evently.ui.common.ArchitectureActivity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.evently.databinding.ActivityOrganizerBinding;
-
-public class OrganizerActivity extends AppCompatActivity {
-
-    private ActivityOrganizerBinding binding;
+public class OrganizerActivity extends ArchitectureActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        binding = ActivityOrganizerBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        // Set the navbar.
-        final var navBar = binding.includeNavbar.navbar;
-        final var fragmentContainer = binding.navHostFragment;
-        NavHostFragment navHostFragment = (NavHostFragment)
-                getSupportFragmentManager().findFragmentById(fragmentContainer.getId());
-        assert navHostFragment != null;
-        NavController navController = navHostFragment.getNavController();
-        NavigationUI.setupWithNavController(navBar, navController);
+    protected int getGraph() {
+        return R.navigation.organizer_graph;
     }
 }
---
+++ app/src/main/java/com/example/evently/ui/organizer/OwnEventsFragment.java
@@ -0,0 +1,28 @@
+package com.example.evently.ui.organizer;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.function.Consumer;
+
+import com.example.evently.data.model.Event;
+import com.example.evently.ui.common.EventsFragment;
+
+public class OwnEventsFragment extends EventsFragment {
+    @Override
+    protected void initEvents(Consumer<List<Event>> callback) {
+        // TODO (chase): Get list of own events by organizer.
+        var placeHolderEvents = new ArrayList<Event>();
+        placeHolderEvents.add(new Event(
+                "Trail Running",
+                "Let's go trail running across the river valley trails!",
+                Instant.parse("2025-11-03T11:59:00.00Z"),
+                Instant.parse("2025-11-09T09:00:00.00Z"),
+                UUID.randomUUID(),
+                Optional.empty(),
+                42));
+        callback.accept(placeHolderEvents);
+    }
+}
