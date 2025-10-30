c7ae3a84f188c3fd68c3bd3d85dd2149b6a90bb6
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2025-10-29 18:28:39 -0600

    Cleanup dependencies and gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,10 @@ android {
         buildConfig = true
         viewBinding = true
     }
+
+    tasks.withType<Test>{
+        useJUnitPlatform()
+    }
 }
 
 spotless {
@@ -89,26 +93,18 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.ext.junit)
-    implementation(libs.runner)
 
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
 
     implementation(libs.credentials)
     implementation(libs.credentials.play.services.auth)
     implementation(libs.googleid)
     implementation(libs.play.services.auth)
-    implementation(libs.legacy.support.v4)
-    implementation(libs.lifecycle.livedata.ktx)
-    implementation(libs.lifecycle.viewmodel.ktx)
 
     testImplementation(libs.junit)
+    androidTestImplementation(libs.runner)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
-    implementation(libs.firebase.firestore)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.junit.v115)
-    androidTestImplementation(libs.espresso.core.v351)
 }
