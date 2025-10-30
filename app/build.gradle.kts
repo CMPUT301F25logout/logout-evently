6f3aadee842f7207deb370e5cf95183ab9cb4637
Alexander <144564614+Alexander-b1@users.noreply.github.com>
2025-10-29 19:30:31 -0600

    Alex/account (#81)
    * Added Account data structure, and began constructing the account DB
    
    * Worked more on the account DB, adding storeAccount. Still need to add fetching of accounts, and add isAdmin status to Account record.
    
    Also, need to get testing working.
    
    * Worked more on the accountDB.
    
    * Began testing accountDB
    
    * Changed accountDB to use the database, rather than a set of accounts.
    
    * Made some changes to account DB, so that we can now get an account from a document snapshot. Also used lambda functions for addOnSuccessListeners, and addOnFailureListeners.
    
    * Finished up Account and AccountDB
    
    * Ran spotlessApply on my code, and updated GIT.md to include the step of checking jdk for spotless.
    
    * Worked on my code, implementing most of the comments. NOTE: currently not working. I don't know why yet, but it is currently not adding information to the DB...
    
    * Worked on my code, implementing most of the comments. NOTE: currently not working. I don't know why yet, but it is currently not adding information to the DB...
    
    * Worked on my code, implementing most of the comments. NOTE: currently not working. I don't know why yet, but it is currently not adding information to the DB...
    
    * Created tests for my code.
    
    * Added a "testDeleteAccount" test method, and looked over code
    
    * Updated code, and improved documentation!
    
    * Resolved merge conflicts.
    
    * Resolved merge conflicts.
    
    * Ran spotless apply
    
    * Remove .idea stuff
    
    * Ignore .vscode stuff
    
    * Fix naming sceheme for instrument test
    
    * Cleanup dependencies and gradle
    
    * Remove explicit junit platform use
    
    ---------
    
    Co-authored-by: TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,6 +92,7 @@ dependencies {
 
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
 
     implementation(libs.credentials)
     implementation(libs.credentials.play.services.auth)
