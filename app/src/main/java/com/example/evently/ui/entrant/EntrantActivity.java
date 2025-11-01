0f7a7156f9d555dcfb00db0cfe2772ac09b871f7
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2025-11-01 14:24:16 -0600

    Navigate to notification on push notif click

--- app/src/main/java/com/example/evently/ui/entrant/EntrantActivity.java
+++ app/src/main/java/com/example/evently/ui/entrant/EntrantActivity.java
@@ -6,8 +6,6 @@ import com.example.evently.R;
 import com.example.evently.ui.common.ArchitectureActivity;
 import com.example.evently.utils.IntentConstants;
 
-import java.util.UUID;
-
 public class EntrantActivity extends ArchitectureActivity {
 
     @Override
@@ -23,11 +21,8 @@ public class EntrantActivity extends ArchitectureActivity {
         var intent = getIntent();
         if (intent.hasExtra(IntentConstants.NOTIFICATION_INTENT_ID_KEY)) {
             // We were sent here by clicking on a notification.
-            // Navigate to the notification page and highlight the notification in question.
-            UUID targetNotificationID = (UUID) intent.getSerializableExtra(IntentConstants.NOTIFICATION_INTENT_ID_KEY);
-            var bundle = new Bundle();
-            bundle.putSerializable(IntentConstants.NOTIFICATION_INTENT_ID_KEY, targetNotificationID);
-            navController.navigate(R.id.nav_notifs, bundle);
+            // Navigate to the notification page and let it handle the intent.
+            navController.navigate(R.id.nav_notifs, null);
         }
     }
 }
--- app/src/main/java/com/example/evently/ui/entrant/ViewNotificationsFragment.java
+++ app/src/main/java/com/example/evently/ui/entrant/ViewNotificationsFragment.java
@@ -7,10 +7,15 @@ import java.util.List;
 import java.util.UUID;
 import java.util.function.Consumer;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.example.evently.data.model.Notification;
 import com.example.evently.ui.common.NotificationsFragment;
+import com.example.evently.utils.IntentConstants;
 
 public class ViewNotificationsFragment extends NotificationsFragment {
     protected void onNotificationClick(Notification notif) {
@@ -35,7 +40,7 @@ public class ViewNotificationsFragment extends NotificationsFragment {
         var seenByOne = new HashSet<String>();
         seenByOne.add("rmaity@ualberta.ca");
         notifs.add(new Notification(
-                UUID.randomUUID(),
+                UUID.fromString("91f1fda5-61ea-4d59-8dab-e6132285290c"),
                 UUID.randomUUID(),
                 Notification.Channel.Winners,
                 "You have been invited to hiking",
@@ -51,5 +56,35 @@ public class ViewNotificationsFragment extends NotificationsFragment {
                 Instant.now(),
                 seenByOne));
         callback.accept(notifs);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // We may have been passed a notification ID to "blink" (double highlight).
+        var intent = requireActivity().getIntent();
+        if (intent.hasExtra(IntentConstants.NOTIFICATION_INTENT_ID_KEY)) {
+            final UUID targetID;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                targetID = intent.getSerializableExtra(
+                        IntentConstants.NOTIFICATION_INTENT_ID_KEY, UUID.class);
+            } else {
+                targetID = (UUID)
+                        intent.getSerializableExtra(IntentConstants.NOTIFICATION_INTENT_ID_KEY);
+            }
+            // Find this notification in the adapter.
+            // NOTE (chase): There is no guarantee that adapter is set by now. It waits on a network
+            // call in onCreateView.
+            // If we notice consistent exceptions here, we must switch to LiveData.
+            // See: https://developer.android.com/topic/libraries/architecture/livedata#java
+            var optionalPos = adapter.findItemPosition(notif -> notif.id().equals(targetID));
+            // Scroll to this notification and highlight it.
+            if (optionalPos.isPresent()) {
+                var targetPos = optionalPos.getAsInt();
+                recyclerView.smoothScrollToPosition(targetPos);
+                // TODO (chase): Is there a way to simulate a "blink" on the notification?
+            }
+        }
     }
 }
