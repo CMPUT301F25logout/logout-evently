211f0c38f8de1a9b7c2906caa25607f8eec34386
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2025-11-06 11:31:37 -0700

    `PromiseOpt` and enhancements

--- app/src/main/java/com/example/evently/data/generic/Promise.java
+++ app/src/main/java/com/example/evently/data/generic/Promise.java
@@ -9,6 +9,8 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import android.util.Pair;
+
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.annotations.concurrent.Background;
@@ -17,8 +19,8 @@ import com.google.firebase.annotations.concurrent.Background;
  * A fluent API for working with firestore concurrent tasks.
  * @param <T> Type of value the promise resolves to.
  */
-public class Promise<T> {
-    private final Task<T> task;
+public sealed class Promise<T> permits PromiseOpt {
+    protected final Task<T> task;
 
     /**
      * Convert a firestore {@link Task} into a Promise, blessing it with a fluent API.
@@ -51,7 +53,7 @@ public class Promise<T> {
         return all(Arrays.stream(promises));
     }
 
-    private Promise(Task<T> task) {
+    protected Promise(Task<T> task) {
         this.task = task;
     }
 
@@ -77,30 +79,78 @@ public class Promise<T> {
     }
 
     /**
-     * Compose one promise with another, using both of their results in the end.
+     * Compose one promise with another, in parallel, using both of their results in the end.
      * @param other The second promise to compose with.
-     * @param act The action to apply with both results.
      * @return A new promise that is a composition of the two.
      * @param <U> Type the second promise resolves to.
+     * @apiNote For the functional programmers, indeed this is the liftA/liftM/ap function.
+     */
+    public <U> Promise<Pair<T, U>> with(Promise<U> other) {
+        final var tasks = Tasks.whenAllSuccess(task, other.task);
+        return promise(tasks.onSuccessTask(results -> {
+            final var thisRes = (T) results.get(0);
+            final var otherRes = (U) results.get(1);
+            return Tasks.forResult(new Pair<>(thisRes, otherRes));
+        }));
+    }
+
+    /**
+     * Compose one promise with another, in parallel, using both of their results in the end.
+     * @param other The second promise to compose with.
+     * @param act An action to apply using both results.
+     * @return A new promise that is a composition of the two with the action applied.
+     * @param <U> Type the second promise resolves to.
+     * @param <R> Type the action returned promise resolves to.
+     * @apiNote For the functional programmers, indeed this is the liftA/liftM/ap function.
+     */
+    public <U, R> Promise<R> with(Promise<U> other, BiFunction<T, U, Promise<R>> act) {
+        return this.with(other).then(pair -> act.apply(pair.first, pair.second));
+    }
+
+    /**
+     * Compose one promise with another, in parallel, using only the second result.
+     * @param other The second promise to compose with.
+     * @return A new promise that is a biased composition of the two.
      * @param <R> Type the returned promise resolves to.
      */
-    public <U, R> Promise<R> compose(Promise<U> other, BiFunction<T, U, Promise<R>> act) {
-        // TODO (chase): This is technically not parallel.
-        //  But using the parallel methods (whenAll) ruins type safety...
-        return promise(
-                task.onSuccessTask(x -> other.task.onSuccessTask(y -> act.apply(x, y).task)));
+    public <R> Promise<R> alongside(Promise<R> other) {
+        final var tasks = Tasks.whenAllSuccess(task, other.task);
+        return promise(tasks.onSuccessTask(results -> {
+            // The second task's result is all that matters and its type is U.
+            final var otherRes = (R) results.get(1);
+            return Tasks.forResult(otherRes);
+        }));
+    }
+
+    /**
+     * Run an action once this promise succeeds.
+     * @apiNote Meant to be called once at the end of a promise chain.
+     *          Using this multiple times will attach several consumers that will all be fired.
+     * @return The same promise we started with, but now there's a success listener.
+     * @param act Consumer to invoke with result.
+     */
+    public Promise<T> thenRun(Consumer<T> act) {
+        return promise(task.addOnSuccessListener(act::accept));
     }
 
     /**
      * Attach an exception handler to the promise.
      * @apiNote Meant to be called once at the end of a promise chain.
      *          Using this multiple times will attach several handlers that will all be fired.
+     * @return The same promise we started with, but now there's a failure listener.
      * @param handler Exception handler.
      */
-    public void catchE(Consumer<Exception> handler) {
-        task.addOnFailureListener(handler::accept);
+    public Promise<T> catchE(Consumer<Exception> handler) {
+        return promise(task.addOnFailureListener(handler::accept));
     }
 
+    /**
+     * Block until a promise finishes and yields the result.
+     * @apiNote NEVER use this on the UI thread.
+     * @return The computed result.
+     * @throws ExecutionException Exceptions thrown during execution of the promise.
+     * @throws InterruptedException Exception thrown due to external interruption of the promise.
+     */
     @Background
     public T await() throws ExecutionException, InterruptedException {
         return Tasks.await(task);
