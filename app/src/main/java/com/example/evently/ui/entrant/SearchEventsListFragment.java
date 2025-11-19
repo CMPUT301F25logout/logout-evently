c0dd7b01c52345c5bbfea0027dc589cec3ea3c4d
YotGun <reganvrn@gmail.com>
2025-11-18 22:36:35 -0700

    Added searchbar for entrant view

---
+++ app/src/main/java/com/example/evently/ui/entrant/SearchEventsListFragment.java
@@ -0,0 +1,53 @@
+package com.example.evently.ui.entrant;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.evently.data.EventsDB;
+import com.example.evently.data.model.Event;
+import com.example.evently.ui.common.EventsFragment;
+import com.google.firebase.Timestamp;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * A fragment representing list of events filtered by the search bar
+ */
+public class SearchEventsListFragment extends EventsFragment {
+
+    /**
+     * Handles clicks on an event row in the Search list.
+     * <p>
+     * Uses the Navigation Component to navigate to the Event Details screen,
+     * passing the clicked event’s ID as a String argument.
+     * @param event The structural representation of the Event view that was clicked.
+     */
+    @Override
+    protected void onEventClick(Event event) {
+        // The action for clicking on the event, pass the event ID to the next event details
+        // fragment
+        var action = HomeFragmentDirections.actionNavHomeToEventDetails(event.eventID());
+        NavController navController = NavHostFragment.findNavController(this);
+        navController.navigate(action);
+    }
+
+    /**
+     * Supplies the Search list with placeholder events
+     * @param callback Callback that will be passed the events into.
+     */
+    @Override
+    protected void initEvents(Consumer<List<Event>> callback) {
+        new EventsDB()
+                .fetchEventsByDate(Timestamp.now(), true)
+                .thenRun(callback)
+                .catchE(e -> {
+                    Log.e("SearchEvents", e.toString());
+                    Toast.makeText(requireContext(), "Something went wrong...", Toast.LENGTH_SHORT)
+                            .show();
+                });
+    }
+}
