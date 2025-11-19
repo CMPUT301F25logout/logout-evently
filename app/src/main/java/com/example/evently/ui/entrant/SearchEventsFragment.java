51513378be1645af393fd3292b9e63968925e6b9
Regan Varian <reganvrn@gmail.com>
2025-11-19 01:47:35 -0700

    Push

--- app/src/main/java/com/example/evently/ui/entrant/SearchEventsFragment.java
+++ app/src/main/java/com/example/evently/ui/entrant/SearchEventsFragment.java
@@ -1,12 +1,10 @@
 package com.example.evently.ui.entrant;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SearchView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
