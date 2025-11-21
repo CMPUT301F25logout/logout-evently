2837f590eb25d342b8f373722339d251f1d40908
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2025-11-21 14:09:34 -0700

    Add `selectWinners` scheduled function

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -6,7 +6,8 @@ import {
 } from "firebase-functions/v2/firestore";
 
 import { initializeApp } from "firebase-admin/app";
-import { FieldPath, getFirestore } from "firebase-admin/firestore";
+import { FieldPath, getFirestore, Timestamp } from "firebase-admin/firestore";
+import { onSchedule } from "firebase-functions/scheduler";
 
 // Constants for safe usage.
 const EVENTS_COLL = "events";
@@ -74,14 +75,54 @@ export const createNotification = onDocumentCreated(
   }
 );
 
+// Select winners for any events past selection time, everyday at midnight.
+export const selectWinners = onSchedule("every day 00:00", async (event) => {
+  const now = new Date();
+  logger.info("Running selection if needed");
+  const allEventsRef = await db.collection(EVENTS_COLL).listDocuments();
+  for (const eventsRef of allEventsRef) {
+    const ref = await eventsRef.get();
+    const eventSelectionTime = ref.get("selectionTime") as Timestamp;
+    const eventSelectionLimit = ref.get("selectionLimit") as number;
+    logger.info("Obtaining information for eventID: " + eventsRef.id);
+    if (eventSelectionTime.toDate() > now) {
+      logger.info("Event enrollment has not ended yet: " + eventsRef.id);
+      continue;
+    }
+    // Run the lottery selection
+    logger.info("Checking selection for eventID: " + eventsRef.id);
+    const eventEntrantsDoc = await db
+      .collection(EVENT_ENTRANTS_COLL)
+      .doc(eventsRef.id)
+      .get();
+    const eventEntrants = eventEntrantsDoc.data() as EventEntrants;
+    if (eventEntrants.selectedEntrants.length != 0) {
+      logger.info("Selection already done eventID: " + eventsRef.id);
+      continue;
+    }
+    if (eventEntrants.enrolledEntrants.length == 0) {
+      logger.info("No entrants for eventID: " + eventsRef.id);
+      continue;
+    }
+    logger.info("Running selection for eventID: " + eventsRef.id);
+    const res = draw(eventEntrants.enrolledEntrants, eventSelectionLimit);
+    await db
+      .collection(EVENT_ENTRANTS_COLL)
+      .doc(eventsRef.id)
+      .update({ selectedEntrants: res });
+  }
+});
+
 class BenignError extends Error {
   constructor(message: string) {
     super(message);
   }
 }
 
-// Redraw winners when someone cancels.
-export const redrawSelected = onDocumentUpdated(
+// Tasks to perform when entrants changes.
+// e.g If someone cancels, we perform a redraw.
+// e.g If winners are added, we write winning notification.
+export const monitorEntrants = onDocumentUpdated(
   `${EVENT_ENTRANTS_COLL}/{eventID}`,
   async (fsEvent) => {
     // Wish there was a way to fire only if the cancelledEntrants field was updated...
@@ -95,38 +136,55 @@ export const redrawSelected = onDocumentUpdated(
     const oldEventEntrants = snapshot.before.data() as EventEntrants;
     const newEventEntrants = snapshot.after.data() as EventEntrants;
 
-    const oldCancelled = new Set(oldEventEntrants.cancelledEntrants);
-    const newCancelled = new Set(newEventEntrants.cancelledEntrants);
-    // Note: New cancelled should strictly be a superset of oldCancelled.
-    // Cancelled entrants are not supposed to be removed, only added.
-    const diff = newCancelled.difference(oldCancelled);
-    if (diff.size === 0) {
-      // No change in cancelled set. Nohing to do.
-      logger.info("Nothing to do");
-      return;
-    }
+    redrawSelected(eventID, oldEventEntrants, newEventEntrants);
+  }
+);
 
-    const eventDoc = await db.collection(EVENTS_COLL).doc(eventID).get();
-    if (!eventDoc.exists) {
-      logger.error(
-        `EventEntrants references non existent event with ID: ${eventID}`
-      );
-      return;
-    }
-    const selectionLimit = eventDoc.get(EVENT_SELECTION_LIMIT_KEY) as number;
+// Redraw winners when someone cancels.
+async function redrawSelected(
+  eventID: string,
+  oldEventEntrants: EventEntrants,
+  newEventEntrants: EventEntrants
+) {
+  logger.info(`Executing redrawSelected for ID: ${eventID}`);
+  const oldCancelled = new Set(oldEventEntrants.cancelledEntrants);
+  const newCancelled = new Set(newEventEntrants.cancelledEntrants);
+  // Note: New cancelled should strictly be a superset of oldCancelled.
+  // Cancelled entrants are not supposed to be removed, only added.
+  const diff = newCancelled.difference(oldCancelled);
+  if (diff.size === 0) {
+    // No change in cancelled set. Nohing to do.
+    logger.info("Nothing to do");
+    return;
+  }
 
-    // Redraw (but be wary of concurrency bugs indeed)!
-    // Note: Multiple instances of this function may be called
-    // around the same time if two users cancel around the same time.
-    // Thus: One must be wise in implementing redraw.
-    const selfRef = db
-      .collection(EVENT_ENTRANTS_COLL)
-      .where(FieldPath.documentId(), "==", eventID)
-      // It's important to be as specific as possible in what we're reading in a transaction.
-      // We don't want the transaction to be retried just because an irrelevant part of the document was updated.
-      .select(entrantsKey("enrolledEntrants"), entrantsKey("selectedEntrants"), entrantsKey("cancelledEntrants"))
-      .limit(1);
-    db.runTransaction(async (tx) => {
+  const eventDoc = await db.collection(EVENTS_COLL).doc(eventID).get();
+  if (!eventDoc.exists) {
+    logger.error(
+      `EventEntrants references non existent event with ID: ${eventID}`
+    );
+    return;
+  }
+  const selectionLimit = eventDoc.get(EVENT_SELECTION_LIMIT_KEY) as number;
+
+  // Redraw (but be wary of concurrency bugs indeed)!
+  // Note: Multiple instances of this function may be called
+  // around the same time if two users cancel around the same time.
+  // Thus: One must be wise in implementing redraw.
+  const selfRef = db
+    .collection(EVENT_ENTRANTS_COLL)
+    .where(FieldPath.documentId(), "==", eventID)
+    // It's important to be as specific as possible in what we're reading in a transaction.
+    // We don't want the transaction to be retried just because an irrelevant part of the document was updated.
+    .select(
+      entrantsKey("enrolledEntrants"),
+      entrantsKey("selectedEntrants"),
+      entrantsKey("cancelledEntrants")
+    )
+    .limit(1);
+
+  return db
+    .runTransaction(async (tx) => {
       const selfEntrantsRes = await tx.get(selfRef);
       if (selfEntrantsRes.empty) {
         throw new Error("EventEntrants selfRef query found no results");
@@ -152,17 +210,20 @@ export const redrawSelected = onDocumentUpdated(
       // Draw for the remaining number of slots.
       const remainingSlots = selectionLimit - eligibleWinners.size;
       const additionalWinners = draw([...eligible], remainingSlots);
+
       // Update the selected entrants list. It should remove the cancelled entrants and add in the new winners.
       tx.update(db.collection(EVENT_ENTRANTS_COLL).doc(eventID), {
-        selectedEntrants: [...eligibleWinners.union(new Set(additionalWinners))],
+        selectedEntrants: [
+          ...eligibleWinners.union(new Set(additionalWinners)),
+        ],
       });
-    }).catch((e) => {
+    })
+    .catch((e) => {
       if (!(e instanceof BenignError)) {
         throw e;
       }
     });
-  }
-);
+}
 
 // Send push notification to devices identified by given tokens.
 async function sendNotification(
@@ -266,5 +327,5 @@ function chooseNRandom<T>(arr: T[], n: number): T[] {
 
 // Type safe way to use the property names of the EventEntrants type as string.
 function entrantsKey(name: keyof EventEntrants) {
-    return name;
+  return name;
 }
