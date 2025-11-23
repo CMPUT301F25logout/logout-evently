a76b1fc2986d1d2d5a95cd5b9c8b56ac58e77264
Alexander Bradley <abradle1@ualberta.ca>
2025-11-22 22:58:14 -0700

    Improving code readability, and code functionality

--- app/src/main/java/com/example/evently/ui/organizer/NotificationThread.java
+++ app/src/main/java/com/example/evently/ui/organizer/NotificationThread.java
@@ -2,12 +2,13 @@ package com.example.evently.ui.organizer;
 
 import java.util.UUID;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.fragment.app.DialogFragment;
 
 import com.google.android.material.button.MaterialButton;
@@ -19,23 +20,20 @@ import com.example.evently.data.model.Notification;
 
 public class NotificationThread extends DialogFragment {
 
-    @SuppressLint("SetTextI18n")
-    @Override
+    private UUID eventID;
+    private Notification.Channel channel;
+    private TextView eventTitle;
+    private TextView eventChannel;
+    private EditText titleText;
+    private EditText descriptionText;
+    private MaterialButton sendNotificationButton;
+
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        // Gets the eventID, and channel from the fragment.
-        Bundle bundle = getArguments();
-        assert bundle != null;
-        UUID eventID = (UUID) bundle.getSerializable("eventID");
-        Notification.Channel channel = (Notification.Channel) bundle.getSerializable("channel");
-        assert eventID != null;
-        assert channel != null;
-
-        // Inflate the layout for this fragment
+        // Inflates the view, and setups up the local variables
         View view = inflater.inflate(R.layout.fragment_notification_thread, container, false);
-        TextView eventTitle = view.findViewById(R.id.tvEventTitle);
-        TextView eventChannel = view.findViewById(R.id.tvNotificationChannel);
+        setupVariables(view);
 
         // Sets the event title, and channel in the thread
         new EventsDB().fetchEvent(eventID).thenRun(event -> {
@@ -48,15 +46,55 @@ public class NotificationThread extends DialogFragment {
         eventChannel.setText(channel.toString());
 
         // Sets up the button to send a notification.
-        MaterialButton sendNotificationButton = view.findViewById(R.id.btnSendNotification);
-        sendNotificationButton.setOnClickListener(v -> {
-            // TODO: Figure out the material TextInputEditText for getting text
 
-            // TODO: Input validation toasts
+        sendNotificationButton.setOnClickListener(v -> {
+            String title = titleText.getText().toString().strip();
+            String description = descriptionText.getText().toString().strip();
+
+            if (title.isEmpty()) {
+                toast("Error: Please enter a title");
+                return;
+            }
+            if (description.isEmpty()) {
+                toast("Error: Please enter a description");
+                return;
+            }
+
+            // If notification is valid, it is sent!
+            new NotificationDB()
+                    .storeNotification(new Notification(
+                            UUID.randomUUID(), eventID, channel, title, description));
+            toast("Notification sent!");
         });
 
         // TODO: Create some sort of notification channel view fragment, which can show the fetched
         new NotificationDB().fetchEventNotifications(eventID, channel);
         return view;
+    }
+
+    /**
+     * Defines the variables used for a notification thread.
+     * @param view The view with the items, like TextViews
+     */
+    private void setupVariables(View view) {
+        // Gets the eventID, and channel from the fragment.
+        Bundle bundle = getArguments();
+        assert bundle != null;
+        eventID = (UUID) bundle.getSerializable("eventID");
+        channel = (Notification.Channel) bundle.getSerializable("channel");
+
+        eventTitle = view.findViewById(R.id.tvEventTitle);
+        eventChannel = view.findViewById(R.id.tvNotificationChannel);
+        titleText = view.findViewById(R.id.etTitle);
+        descriptionText = view.findViewById(R.id.etDescription);
+        sendNotificationButton = view.findViewById(R.id.btnSendNotification);
+    }
+
+    /**
+     * Shows a short-length {@link Toast} with the given message in this Fragment's context
+     * @param msg message to display to the user
+     */
+    private void toast(String msg) {
+        Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show();
     }
 }
