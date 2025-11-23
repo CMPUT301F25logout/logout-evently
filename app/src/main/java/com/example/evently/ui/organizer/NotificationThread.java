f1c82592f90c7b58c5d7aff046f4f6379607b036
Alexander Bradley <abradle1@ualberta.ca>
2025-11-22 18:32:23 -0700

    Bit more work on threads

--- app/src/main/java/com/example/evently/ui/organizer/NotificationThread.java
+++ app/src/main/java/com/example/evently/ui/organizer/NotificationThread.java
@@ -2,6 +2,7 @@ package com.example.evently.ui.organizer;
 
 import java.util.UUID;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,11 +13,13 @@ import androidx.fragment.app.DialogFragment;
 import com.google.android.material.button.MaterialButton;
 
 import com.example.evently.R;
+import com.example.evently.data.EventsDB;
 import com.example.evently.data.NotificationDB;
 import com.example.evently.data.model.Notification;
 
 public class NotificationThread extends DialogFragment {
 
+    @SuppressLint("SetTextI18n")
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -26,29 +29,34 @@ public class NotificationThread extends DialogFragment {
         assert bundle != null;
         UUID eventID = (UUID) bundle.getSerializable("eventID");
         Notification.Channel channel = (Notification.Channel) bundle.getSerializable("channel");
+        assert eventID != null;
+        assert channel != null;
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_notification_thread, container, false);
-
         TextView eventTitle = view.findViewById(R.id.tvEventTitle);
         TextView eventChannel = view.findViewById(R.id.tvNotificationChannel);
 
-        // TODO: Show the eventTitle, and eventChannel (eventTitle needs to be fetched)
+        // Sets the event title, and channel in the thread
+        new EventsDB().fetchEvent(eventID).thenRun(event -> {
+            if (event.isPresent()) {
+                eventTitle.setText(event.get().name());
+            } else {
+                eventTitle.setText("Event not found!");
+            }
+        });
+        eventChannel.setText(channel.toString());
 
+        // Sets up the button to send a notification.
         MaterialButton sendNotificationButton = view.findViewById(R.id.btnSendNotification);
-
         sendNotificationButton.setOnClickListener(v -> {
             // TODO: Figure out the material TextInputEditText for getting text
 
             // TODO: Input validation toasts
         });
 
-        assert eventID != null;
-        assert channel != null;
-        new NotificationDB().fetchEventNotifications(eventID, channel);
-
         // TODO: Create some sort of notification channel view fragment, which can show the fetched
-        // notifications for the channel.
+        new NotificationDB().fetchEventNotifications(eventID, channel);
         return view;
     }
 }
