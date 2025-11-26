23ed8d88ced19c837514c7b14c2034c1c6559170
Ronan St. Amand <rstamand@ualberta.ca>
2025-11-25 20:22:44 -0700

    Dropdown for sending notifications

--- app/src/main/java/com/example/evently/ui/organizer/OrganizerEventActionsFragment.java
+++ app/src/main/java/com/example/evently/ui/organizer/OrganizerEventActionsFragment.java
@@ -3,19 +3,29 @@ package com.example.evently.ui.organizer;
 import static com.example.evently.data.model.Notification.winnerNotification;
 
 import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.evently.data.NotificationDB;
+import com.example.evently.data.model.Notification;
 import com.example.evently.databinding.FragmentOrganizerEventActionsBinding;
+import com.example.evently.databinding.FragmentOrganizerEventActionsMenuBinding;
 import com.example.evently.ui.common.EventQRDialogFragment;
+import com.example.evently.ui.entrant.EntrantActivity;
 import com.example.evently.ui.model.EventViewModel;
 
 /**
@@ -23,10 +33,12 @@ import com.example.evently.ui.model.EventViewModel;
  * This takes care of setting up the action buttons for the organizer view.
  * MUST be used by {@link EditEventDetailsFragment } as it requires ViewModels from there.
  */
-public class OrganizerEventActionsFragment extends Fragment {
-    private FragmentOrganizerEventActionsBinding binding;
-
+public class OrganizerEventActionsFragment extends Fragment
+        implements AdapterView.OnItemSelectedListener {
+    private FragmentOrganizerEventActionsMenuBinding binding;
+    private Notification.Channel currentlySelectedChannel = Notification.Channel.All;
     private EventViewModel eventViewModel;
+    private final List<Notification.Channel> channels = Arrays.asList(Notification.Channel.values());
 
     @Override
     public View onCreateView(
@@ -34,10 +46,22 @@ public class OrganizerEventActionsFragment extends Fragment {
             @Nullable ViewGroup container,
             @Nullable Bundle savedInstanceState) {
         binding =
-                FragmentOrganizerEventActionsBinding.inflate(getLayoutInflater(), container, false);
+                FragmentOrganizerEventActionsMenuBinding.inflate(getLayoutInflater(), container, false);
 
         eventViewModel = new ViewModelProvider(requireParentFragment()).get(EventViewModel.class);
 
+        final var channels = Notification.Channel.values();
+
+        ArrayAdapter<Notification.Channel> adapter = new ArrayAdapter<>(
+                this.getContext(),
+                android.R.layout.simple_spinner_item,
+                channels
+        );
+
+        binding.notifChannelSpinner.setAdapter(adapter);
+        binding.notifChannelSpinner.setOnItemSelectedListener(this);
+        binding.notifChannelSpinner.setSelection(0);
+        //TODO: Move out of material button layout, make a linearlayout with spinner and buttons
         return binding.getRoot();
     }
 
@@ -45,24 +69,29 @@ public class OrganizerEventActionsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        eventViewModel.getEventLive().observe(getViewLifecycleOwner(), event -> {
-            if (event.selectionTime().toInstant().isBefore(Instant.now())) {
-                binding.notifySelected.setEnabled(true);
-                binding.notifySelected.setOnClickListener(v -> {
-                    binding.notifySelected.setEnabled(false);
-                    new NotificationDB().storeNotification(winnerNotification(event.eventID()));
-                });
-            } else {
-                binding.notifySelected.setEnabled(false);
-            }
-        });
-
-        binding.utilShareBtn.shareBtn.setOnClickListener(v -> {
+        binding.materialButtons.utilShareBtn.shareBtn.setOnClickListener(v -> {
             final var qrDialog = new EventQRDialogFragment();
             final var bundle = new Bundle();
             bundle.putSerializable("eventID", eventViewModel.eventID);
             qrDialog.setArguments(bundle);
             qrDialog.show(getChildFragmentManager(), "QR_DIALOG");
         });
+
+        binding.materialButtons.sendNotification.setOnClickListener(this::sendNotification);
     }
+
+    private void sendNotification(View view) {
+        final var bundle = new Bundle();
+        bundle.putSerializable("eventID", eventViewModel.eventID);
+        bundle.putSerializable("channel", currentlySelectedChannel);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if (currentlySelectedChannel.ordinal() == position) return;
+        currentlySelectedChannel = channels.get(position);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {}
 }
