5458a67ac430182935b8708e128d8e5c0a06a317
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2025-11-28 15:19:16 -0700

    Update tests as per event structure change

--- app/src/androidTest/java/com/example/evently/NotificationThreadTest.java
+++ app/src/androidTest/java/com/example/evently/NotificationThreadTest.java
@@ -51,6 +51,7 @@ public class NotificationThreadTest extends EmulatedFragmentTest<NotificationThr
             "name",
             "description",
             Category.EDUCATIONAL,
+            false,
             selectionTime,
             eventTime,
             mockAccount.email(),
