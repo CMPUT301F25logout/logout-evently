fdd7c7c013cb8b954db482510caebe932f280ae4
Alexander Bradley <abradle1@ualberta.ca>
2025-11-27 21:21:37 -0700

    Started working on the selected to canceled functionality

--- app/src/main/java/com/example/evently/ui/common/EntrantRecyclerViewAdapter.java
+++ app/src/main/java/com/example/evently/ui/common/EntrantRecyclerViewAdapter.java
@@ -2,6 +2,7 @@ package com.example.evently.ui.common;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
@@ -17,13 +18,22 @@ import com.example.evently.databinding.FragmentEntrantBinding;
 public class EntrantRecyclerViewAdapter
         extends RecyclerView.Adapter<EntrantRecyclerViewAdapter.EntrantViewHolder> {
     private final List<String> entrants;
+    private final boolean showRemoveButton;
+    private UUID eventID;
 
     public EntrantRecyclerViewAdapter() {
         this.entrants = new ArrayList<>();
+        showRemoveButton = false;
     }
 
     public EntrantRecyclerViewAdapter(List<String> entrants) {
         this.entrants = entrants;
+        showRemoveButton = false;
+    }
+
+    public EntrantRecyclerViewAdapter(List<String> entrants, UUID eventID) {
+        this.entrants = entrants;
+        showRemoveButton = true;
     }
 
     public static class EntrantViewHolder extends RecyclerView.ViewHolder {
@@ -49,6 +59,18 @@ public class EntrantRecyclerViewAdapter
             return;
         }
         var binding = holder.binding;
+
+        // TODO: Add a remove button with default invisibility to the XML
+
+        if (showRemoveButton) {
+
+            // TODO: Change visibility in this section
+
+            // TODO Add an onclick listener to delete the user from the selectedEntrants,
+            //  and move to canceled
+
+        }
+
         // Define the names
         String name = entrants.get(position);
 
