46a90690b570d48b1952ce6ea3c484ed9079f26a
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2025-11-29 15:06:48 -0700

    Fix `testSwitchingToEventDetails`

--- app/src/androidTest/java/com/example/evently/BrowseToEventDetailsTest.java
+++ app/src/androidTest/java/com/example/evently/BrowseToEventDetailsTest.java
@@ -10,6 +10,7 @@ import static org.junit.Assert.assertNotNull;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.concurrent.ExecutionException;
 
@@ -39,7 +40,7 @@ public class BrowseToEventDetailsTest extends EmulatedFragmentTest<BrowseEventsF
 
     private static final Instant now = Instant.now();
     // We can use the same times for these tests.
-    private static final Timestamp selectionTime = new Timestamp(now.plus(Duration.ofMillis(100)));
+    private static final Timestamp selectionTime = new Timestamp(now.plus(Duration.ofMinutes(5)));
     private static final Timestamp eventTime = new Timestamp(now.plus(Duration.ofMinutes(10)));
 
     // Create a few events.
@@ -64,14 +65,13 @@ public class BrowseToEventDetailsTest extends EmulatedFragmentTest<BrowseEventsF
     public void testSwitchingToEventDetails() throws InterruptedException {
         Thread.sleep(2000);
         final DateTimeFormatter some_date =
-                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(ZoneId.of("UTC"));
+                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(ZoneId.systemDefault());
 
         Event expectedEvent = mockEvents[0];
 
         assertRecyclerViewItem(
                 R.id.event_list,
                 p(R.id.content, expectedEvent.name()),
-                p(R.id.txtselection_date, "Waitlist closed"),
                 p(R.id.txtDate, some_date.format(expectedEvent.eventTime().toInstant())));
 
         onView(withId(R.id.btnDetails)).perform(ViewActions.click());
