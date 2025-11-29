fa11acbd5cf6c44a28388e0cb5683dd6ee819825
Ronan St. Amand <rstamand@ualberta.ca>
2025-11-29 16:44:42 -0700

    csv created and downloaded

--- app/src/androidTest/java/com/example/evently/OrganizerEventActionsTest.java
+++ app/src/androidTest/java/com/example/evently/OrganizerEventActionsTest.java
@@ -1,16 +1,19 @@
 package com.example.evently;
 
-import com.example.evently.data.model.Category;
-import com.example.evently.data.model.Event;
-import com.example.evently.ui.organizer.OrganizerEventActionsFragment;
-import com.google.firebase.Timestamp;
-
 import java.time.Duration;
 import java.time.Instant;
 
+import com.google.firebase.Timestamp;
+
+import com.example.evently.data.model.Category;
+import com.example.evently.data.model.Event;
+import com.example.evently.ui.organizer.OrganizerEventActionsFragment;
+
 public class OrganizerEventActionsTest extends EmulatedFragmentTest<OrganizerEventActionsFragment> {
     @Override
-    protected int getGraph() {return R.navigation.organizer_graph;}
+    protected int getGraph() {
+        return R.navigation.organizer_graph;
+    }
 
     @Override
     protected Class<OrganizerEventActionsFragment> getFragmentClass() {
--- app/src/main/java/com/example/evently/ui/organizer/OrganizerEventActionsFragment.java
+++ app/src/main/java/com/example/evently/ui/organizer/OrganizerEventActionsFragment.java
@@ -3,13 +3,16 @@ package com.example.evently.ui.organizer;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.PopupMenu;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,8 +20,8 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.evently.data.EventsDB;
 import com.example.evently.data.model.Notification;
-import com.example.evently.data.model.EventEntrants;
 import com.example.evently.databinding.FragmentOrganizerEventActionsBinding;
 import com.example.evently.ui.common.EventQRDialogFragment;
 import com.example.evently.ui.model.EventViewModel;
@@ -32,8 +35,8 @@ public class OrganizerEventActionsFragment extends Fragment {
     private FragmentOrganizerEventActionsBinding binding;
     private String currentlySelectedChannel = Notification.Channel.All.name();
     private EventViewModel eventViewModel;
-    private EventEntrants entrantsDB;
-    private static final int CREATE_FILE = 1;
+    private ActivityResultLauncher<Intent> createFile;
+    private List<String> accepted;
 
     @Override
     public View onCreateView(
@@ -46,7 +49,29 @@ public class OrganizerEventActionsFragment extends Fragment {
 
         eventViewModel = new ViewModelProvider(requireParentFragment()).get(EventViewModel.class);
 
-        entrantsDB = new EventEntrants(eventViewModel.eventID);
+        createFile = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(), result -> {
+                    Log.i("CSV", "CSV write launched");
+                    if (result.getResultCode() != OrganizerActivity.RESULT_OK) {
+                        Log.w("CSV", "Result bad. code: " + result.getResultCode());
+                        return; // May need to change returns to making a toast
+                    }
+
+                    if (result.getData() == null) return;
+                    var uri = result.getData().getData();
+                    if (uri == null) {
+                        Log.w("CSV", "URI was null");
+                        return;
+                    }
+                    try (OutputStream os =
+                            requireContext().getContentResolver().openOutputStream(uri)) {
+                        os.write(String.join(",", accepted).getBytes(StandardCharsets.UTF_8));
+                        os.flush();
+                        Log.i("CSV", "Exported CSV Successfully");
+                    } catch (IOException e) {
+                        Log.e("CSV", "CSV write error", e);
+                    }
+                });
 
         return binding.getRoot();
     }
@@ -70,8 +95,14 @@ public class OrganizerEventActionsFragment extends Fragment {
                 .navigate(EditEventDetailsFragmentDirections.actionEventDetailsToNavThread(
                         eventViewModel.eventID, currentlySelectedChannel)));
 
-        binding.exportEntrants.setClickable(!entrantsDB.accepted().isEmpty());
-        binding.exportEntrants.setOnClickListener(this::exportCSV);
+        binding.exportEntrants.setEnabled(false);
+        var eventsDB = new EventsDB();
+        eventsDB.fetchEventEntrants(eventViewModel.eventID).optionally(entrants -> {
+            accepted = entrants.accepted();
+            if (accepted.isEmpty()) return;
+            binding.exportEntrants.setEnabled(true);
+            binding.exportEntrants.setOnClickListener(this::exportCSV);
+        });
     }
 
     @Override
@@ -101,39 +132,13 @@ public class OrganizerEventActionsFragment extends Fragment {
         popup.show();
     }
 
-    private void exportCSV(View v) {
-        if (entrantsDB.accepted().isEmpty()) return;
-
+    private void exportCSV(View view) {
+        Log.i("CSV", "CSV write called");
         Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType("text/csv");
         intent.putExtra(Intent.EXTRA_TITLE, "entrants.csv");
 
-        registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
-                    if (result.getResultCode() != OrganizerActivity.RESULT_OK)
-                        return; // May need to change returns to making a toast
-
-                    if (result.getData() == null) return;
-                    var uri = result.getData().getData();
-                    if (uri == null) return;
-
-                    var csvString = String.join(",", entrantsDB.accepted());
-
-                    try (OutputStream os =
-                            requireContext().getContentResolver().openOutputStream(uri)) {
-                        os.write(csvString.getBytes(StandardCharsets.UTF_8));
-                        os.flush();
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                })
-                .launch(intent);
-
-        binding.sendNotif.setText(String.format("Notify %s", currentlySelectedChannel));
-        binding.selectChannel.setCheckable(true);
-        binding.selectChannel.setOnClickListener(this::selectChannel);
-        binding.sendNotif.setOnClickListener(v -> NavHostFragment.findNavController(this)
-                .navigate(EditEventDetailsFragmentDirections.actionEventDetailsToNavThread(
-                        eventViewModel.eventID, currentlySelectedChannel)));
+        createFile.launch(intent);
     }
 }
