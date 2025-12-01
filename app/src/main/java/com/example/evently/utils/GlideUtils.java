3f37a96aed7d48921e0fdf112d2ca4ecd3e1aa62
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2025-12-01 16:15:02 -0700

    Handle glide when image loading on top of default image

--- app/src/main/java/com/example/evently/utils/GlideUtils.java
+++ app/src/main/java/com/example/evently/utils/GlideUtils.java
@@ -1,8 +1,15 @@
 package com.example.evently.utils;
 
+import android.graphics.drawable.Drawable;
 import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
+
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.bumptech.glide.signature.ObjectKey;
 import com.google.firebase.storage.StorageReference;
 
@@ -38,8 +45,18 @@ public class GlideUtils {
             // Loads image into provided ImageView
             Glide.with(imageView.getContext())
                     .load(posterReference)
-                    .placeholder(android.R.drawable.ic_menu_report_image)
-                    .error(android.R.drawable.ic_menu_report_image)
+                    .listener(new RequestListener<Drawable>() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            imageView.setImageTintList(null);
+                            return false;
+                        }
+                    })
                     .signature(new ObjectKey(lastUpdated)) // Updates cache if changed
                     .into(imageView);
         });
