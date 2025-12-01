07c0fed03794865da95bd02cb66f8d04c2bde44b
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2025-12-01 15:52:54 -0700

    Better notification dialog

--- app/src/main/java/com/example/evently/ui/entrant/NotificationGenericDialog.java
+++ app/src/main/java/com/example/evently/ui/entrant/NotificationGenericDialog.java
@@ -2,15 +2,22 @@ package com.example.evently.ui.entrant;
 
 import java.util.UUID;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import com.example.evently.R;
 import com.example.evently.data.EventsDB;
 import com.example.evently.data.NotificationDB;
+import com.example.evently.databinding.FragmentNotifBinding;
 import com.example.evently.utils.FirebaseAuthUtils;
 
 /**
@@ -18,46 +25,60 @@ import com.example.evently.utils.FirebaseAuthUtils;
  * These simply have an "ok" button. No further action is required.
  */
 public class NotificationGenericDialog extends DialogFragment {
+
+    private FragmentNotifBinding binding;
+
+    private UUID eventID;
+    private String message;
+
     @NonNull @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
-
         var args = getArguments();
         if (args == null) {
             // This should never happen.
-            return builder.create();
+            throw new IllegalArgumentException("Notification dialog opened without arguments");
         }
+
+        binding = FragmentNotifBinding.inflate(getLayoutInflater(), null, false);
 
         var notificationID = (UUID) args.getSerializable("id");
         assert notificationID != null;
-        var eventID = (UUID) args.getSerializable("eventID");
+        eventID = (UUID) args.getSerializable("eventID");
         var title = args.getString("title");
-        var message = args.getString("description");
-        builder.setTitle(title)
-                .setMessage(message)
+        message = args.getString("description");
+
+        return new MaterialAlertDialogBuilder(requireContext())
+                .setTitle(title)
+                .setView(binding.getRoot())
                 .setPositiveButton("OK", (dialog, id) -> new NotificationDB()
                         .markSeen(notificationID, FirebaseAuthUtils.getCurrentEmail()))
-                .setNeutralButton("See Details", (dialog, id) -> {
-                    if (eventID != null) {
-                        NavHostFragment.findNavController(requireParentFragment())
-                                .navigate(
-                                        ViewNotificationsFragmentDirections
-                                                .actionNavNotifsToEventDetails(eventID));
-                    }
-                    new NotificationDB()
-                            .markSeen(notificationID, FirebaseAuthUtils.getCurrentEmail());
-                });
+                .setNeutralButton("View Event", (dialog, id) -> {
+                    NavHostFragment.findNavController(requireParentFragment())
+                            .navigate(
+                                    ViewNotificationsFragmentDirections
+                                            .actionNavNotifsToEventDetails(eventID));
+                })
+                .create();
+    }
 
-        final var alertDialog = builder.create();
+    @Nullable @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        return binding.getRoot();
+    }
 
-        if (eventID != null) {
-            new EventsDB()
-                    .fetchEvent(eventID)
-                    .thenRun(eventOpt -> eventOpt.ifPresent(event -> requireActivity()
-                            .runOnUiThread(() -> alertDialog.setMessage(
-                                    "You have been invited to the event:\n" + event.name()))));
-        }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        return alertDialog;
+        new EventsDB()
+                .fetchEvent(eventID)
+                .thenRun(eventOpt -> eventOpt.ifPresent(event -> {
+                    final var msg = getString(R.string.event_dialog_msg, event.name(), message);
+                    binding.notifDescription.setText(msg);
+                }));
+        binding.notifDescription.setText(message);
     }
 }
--- app/src/main/java/com/example/evently/ui/entrant/NotificationWinnerDialog.java
+++ app/src/main/java/com/example/evently/ui/entrant/NotificationWinnerDialog.java
@@ -2,14 +2,22 @@ package com.example.evently.ui.entrant;
 
 import java.util.UUID;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import com.example.evently.R;
 import com.example.evently.data.EventsDB;
 import com.example.evently.data.NotificationDB;
+import com.example.evently.databinding.FragmentNotifBinding;
 import com.example.evently.utils.FirebaseAuthUtils;
 
 /**
@@ -17,26 +25,35 @@ import com.example.evently.utils.FirebaseAuthUtils;
  * These not only show the notification title, message etc. but also the "accept or decline" buttons.
  */
 public class NotificationWinnerDialog extends DialogFragment {
+    private FragmentNotifBinding binding;
+
+    private UUID eventID;
+    private String message;
+
+    private EventsDB eventsDB = new EventsDB();
+    private NotificationDB notificationDB = new NotificationDB();
+
     @NonNull @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
-        final var eventsDB = new EventsDB();
-        final var notificationDB = new NotificationDB();
-        final var self = FirebaseAuthUtils.getCurrentEmail();
-
         var args = getArguments();
         if (args == null) {
             // This should never happen.
-            return builder.create();
+            throw new IllegalArgumentException("Notification dialog opened without arguments");
         }
 
+        final var self = FirebaseAuthUtils.getCurrentEmail();
+
+        binding = FragmentNotifBinding.inflate(getLayoutInflater(), null, false);
+
         var notificationID = (UUID) args.getSerializable("id");
-        var eventID = (UUID) args.getSerializable("eventID");
         assert notificationID != null;
+        eventID = (UUID) args.getSerializable("eventID");
         var title = args.getString("title");
-        var message = args.getString("description");
-        builder.setTitle(title)
-                .setMessage(message)
+        message = args.getString("description");
+
+        return new MaterialAlertDialogBuilder(requireContext())
+                .setTitle(title)
+                .setView(binding.getRoot())
                 .setPositiveButton("Accept", (dialog, id) -> {
                     eventsDB.addAccepted(eventID, self);
                     notificationDB.markSeen(notificationID, self);
@@ -44,7 +61,34 @@ public class NotificationWinnerDialog extends DialogFragment {
                 .setNegativeButton("Decline", (dialog, id) -> {
                     eventsDB.addCancelled(eventID, self);
                     notificationDB.markSeen(notificationID, FirebaseAuthUtils.getCurrentEmail());
-                });
-        return builder.create();
+                })
+                .setNeutralButton("View Event", (dialog, id) -> {
+                    NavHostFragment.findNavController(requireParentFragment())
+                            .navigate(
+                                    ViewNotificationsFragmentDirections
+                                            .actionNavNotifsToEventDetails(eventID));
+                })
+                .create();
+    }
+
+    @Nullable @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        new EventsDB()
+                .fetchEvent(eventID)
+                .thenRun(eventOpt -> eventOpt.ifPresent(event -> {
+                    final var msg = getString(R.string.event_dialog_msg, event.name(), message);
+                    binding.notifDescription.setText(msg);
+                }));
+        binding.notifDescription.setText(message);
     }
 }
